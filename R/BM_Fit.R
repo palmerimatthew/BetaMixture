ee7f7b5971a4ff530c7e5c0a9bf1405a92ae9767
palmerimatthew <37392128+palmerimatthew@users.noreply.github.com>
2020-07-26 14:46:03 -0500

    Finished E_Step Function

--- R/BM_Fit.R
+++ R/BM_Fit.R
@@ -8,8 +8,82 @@
 #' @return TODO
 #' @export
 BM_Fit <- function(x, K, threshold, seed = NA) {
-  #setting seed if seed is not NA
+  #TODO: pre-condition tests
+
+  #setup ----
   if (!is.na(seed)){set.seed(seed)}
+  Params = seq(0, 1, length.out = K+1)[2]
+  Mix_Params = rep(Mix_Params, K)
+  Component_Params = MoM_Calculation(sort(x), K, length(x))
+  Alphas = Component_Params[seq(1, length(Component_Params), by=2)]
+  Betas = Component_Params[seq(2, length(Component_Params), by=2)]
+  current_log_likelihood = sum(BM_Density(x, Mix_Params, Alphas, Betas, T))
+  previous_log_likelihood = current_log_likelihood - 100
+  #record of iterations
+  iteration_results = matrix(nrow = 100, ncol = 2+3*K)
+  iteration = 1
+  iteration_results[1,] = c(iteration, Mix_Params, Alphas, Betas, current_log_likelihood)
+  #starting iterative process ----
+  while (current_log_likelihood - previous_log_likelihood > threshold) {
+    iteration = iteration + 1
+    Mix_Params = E_Step(x, Alphas, Betas)
+    Component_Params = M_Step(x, Mix_Params, Component_Params)
+    Alphas = Component_Params[seq(1, length(Component_Params), by=2)]
+    Betas = Component_Params[seq(1, length(Component_Params), by=2)]
+    previous_log_likelihood = current_log_likelihood
+    current_log_likelihood = sum(BM_Density(x, Mix_Params, Alphas, Betas, T))
+    iteration_results[2,] = c(iteration, Mix_Params, Alphas, Betas, current_log_likelihood)
+  }
+}
 
+#Expectation step ----
+E_Step <- function(x, Alphas, Betas) {
+  K = length(Alphas)
+  responsibility = matrix(nrow = K, ncol = length(x))
+  #matrix of responsibility numerators (KxN)
+  for (i in 1:K) {
+    responsibility[i,] = dbeta(x, Alphas[i], Betas[i])
+  }
+  respons_denom = colSums(responsibility)
+  #responsibility matrix (NxK)
+  responsibility = t(responsibility)/respons_denom
+  Mixture_Params = colSums(responsibility)/sum(responsibility)
+  return(Mixture_Params)
+}
 
+#Maximization step ----
+M_Step <- function(x, Mixture_Params, Old_Component_Params) {
+
+}
+
+# Method of Moments initial values for component parameters ----
+MoM_Calculation <- function(sorted_x, K, len) {
+  cut_points = seq(1, len, length.out = K+1)
+  MoM_Estimates = c()
+  for(i in 1:K) {
+    temp_data = sorted_x[ceiling(cut_points[i]):floor(cut_points[i+1])]
+    temp <- MoM_Beta(temp_data)
+    alpha = temp$alpha
+    beta = temp$beta
+    MoM_Estimates = c(MoM_Estimates, c(alpha, beta))
+  }
+  return(MoM_Estimates)
+}
+
+MoM_Beta <- function(data) {
+  mean = mean(data)
+  var = var(data)
+  intermediate = mean*(1-mean)/var - 1
+  alpha = mean*intermediate
+  beta = (1-mean)*intermediate
+  return(list(alpha = alpha, beta = beta))
+}
+
+#Testing ----
+Test_BM_Fit <- function() {
+  #Precondition tests for BM_Fit ----
+  print("BM_Fit tests not implemented yet")
+
+  #MoM_Calculation tests ----
+  print("MoM_Calculation tests not implemented yet")
 }
